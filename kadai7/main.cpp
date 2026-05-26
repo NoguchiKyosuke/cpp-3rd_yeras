c7ca84ae8820b300d7badcbdbf9aff90f1e5164a
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-05-26 16:49:08 +0900

    craete kadai7

---
+++ kadai7/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "Population.h"
+
+
+int main(int args, char* argv[]) {
+    Population *pop[2]; // Populationのオブジェクト
+
+    pop[0] = new Population();
+    pop[1] = new Population();
+
+    pop[0]->read("Aichi.txt"); // データファイルからデータを読み込む
+    pop[1]->read("Zenkoku.txt"); // データファイルからデータを読み込む
+    *pop[0] % *pop[1]; // 2つのPopulationオブジェクトを比較して、就業人口の最大値を取得する。比較演算子%はPopulationクラスで定義されている。
+    int maxN = ! *pop[0]; // 就業人口の最大値を取得する
+    std::string maxLabel = *pop[0] ^ maxN;
+    std::cout << maxLabel << std::endl;
+}
