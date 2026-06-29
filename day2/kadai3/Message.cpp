5900fd4bad19c780dc973e62a90a50f3fc4b028a
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-06-29 21:24:14 +0900

    include iostream

--- day2/kadai3/Message.cpp
+++ day2/kadai3/Message.cpp
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-#include <string>
 #include "Message.h"
 
 Message::Message(): message() {
--- day2/kadai3/main.cpp
+++ day2/kadai3/main.cpp
@@ -4,5 +4,7 @@
 int main (int argc, char *argv[]) {
   Message obj1("Hello, World!");
 
+  obj1.showAllMessages();
+
   return 0;
 }
