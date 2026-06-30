1c40ff567464548265dc7792d165f94695d479b7
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-06-30 16:19:08 +0900

    finish day3-kadai1

---
+++ day3/kadai2/Message.cpp
@@ -0,0 +1,32 @@
+#include "Message.h"
+
+Message::Message(): message() {
+}
+
+Message::Message(const std::string& message_string) {
+  message.push_back(message_string);
+}
+
+Message::Message(const std::vector<std::string>& message_vector) : message(message_vector) {
+}
+
+Message::~Message() {
+}
+
+void Message::addMessage (const std::string& message_string) {
+  message.push_back(message_string);
+}
+
+std::string Message::getMessage (int message_id) {
+  return message[message_id];
+}
+
+void Message::showAllMessages (void) {
+  for (size_t i = 0; i < message.size(); ++i) {
+    std::cout << message[i] << std::endl;
+  }
+}
+
+int Message::getNMessages(void) {
+  return message.size();
+}
