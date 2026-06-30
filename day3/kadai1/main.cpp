b8a01c8ee500cee23baa5877bad48e3e441a25bc
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-06-30 15:32:10 +0900

    start day3

---
+++ day3/kadai1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include "Message.h"
+
+int main (int argc, char *argv[]) {
+  printf("コンストラクタの呼び出しとshowAllMessages()のテスト\n");
+  Message obj1("Hello, World!");
+  obj1.showAllMessages();
+
+  printf("addMessage()の呼び出しのテスト\n");
+  obj1.addMessage("additional message.");
+  obj1.showAllMessages();
+
+  printf("getMessage()の呼び出しのテスト\n");
+  std::cout << "Message 0: " << obj1.getMessage(0) << std::endl;
+  std::cout << "Message 1: " << obj1.getMessage(1) << std::endl;
+
+  printf("vectorクラスを引数にとるオブジェクトの生成の呼び出しのテスト\n");
+  std::vector<std::string> messages = {"Message 1", "Message 2", "Message 3"};
+  Message obj2(messages);
+  obj2.showAllMessages();
+
+  printf("getNMessages()の呼び出しのテスト\n");
+  std::cout << "Number of messages in obj1: " << obj1.getNMessages() << std::endl;
+
+  return 0;
+}
