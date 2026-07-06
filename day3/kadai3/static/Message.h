5e51f88b485fb33055dda17fb6e34e5f8ef259ba
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-07-06 14:28:52 +0900

    start day3-kadai3

---
+++ day3/kadai3/static/Message.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Message {
+private:
+  std::vector<std::string> message;
+
+public:
+  Message();
+  Message(const std::string& message_string);
+  Message(const std::vector<std::string>& message_vector);
+  ~Message();
+
+  void addMessage (const std::string& message_string);
+  std::string getMessage (int message_id);
+  void showAllMessages (void);
+  int getNMessages(void);
+};
