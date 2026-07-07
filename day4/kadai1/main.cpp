6a223b58f92b6efee90a4aac89f390615c19d397
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-07-07 15:35:19 +0900

    start day4

---
+++ day4/kadai1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <random>
+
+int main (int argc, char *argv[]) {
+  // 平均値0, 分散1として定義
+  double average = 0.0;
+  double variance = 1.0;
+  int N[] = {10, 100, 1000, 10000, 100000};
+
+  std::uint32_t seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::mt19937 method(seed);
+
+  std::normal_distribution<double> dist(average, variance);
+
+  double val = dist(method);
+
+  printf("平均値: %f, 分散: %f, N: %d, 乱数: %f\n", average, variance, N[0], val);
+
+  return 0;
+}
