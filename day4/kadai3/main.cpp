c37452dcc1cacaa6ccbee1087a1b2c461d9f0327
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-07-13 21:08:22 +0900

    write day4-kadai3

--- day4/kadai3/main.cpp
+++ day4/kadai3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <random>
+#include <fstream>
 
 int main (int argc, char *argv[]) {
   // N, σの値を設定する。
@@ -11,12 +12,12 @@ int main (int argc, char *argv[]) {
   Eigen::VectorXd x(N);
   Eigen::VectorXd y(N);
   for (int i = 1; i <= N; i++){
-    x(i - 1) = -10 + (20 / (N - 1)) * (i - 1);
-    y(i - 1) = (3 * x(i - 1) + 4) / 2;
+    x(i - 1) = -10.0 + (20.0 / (N - 1)) * (i - 1);
+    y(i - 1) = (3.0 * x(i - 1) + 4.0) / 2.0;
   }
 
-  // 平均値, 分散、データ数をコマンドライン引数から取得
-  double ave = 0;
+  // 正規分布の平均値を0.0として定義する。
+  double ave = 0.0;
 
   // シード値を1として、乱数生成器を初期化
   std::uint32_t seed = 1;
@@ -26,12 +27,18 @@ int main (int argc, char *argv[]) {
   std::normal_distribution<double> dist(ave, sigma);
 
   // N個の乱数を生成してxとyに加える
+  Eigen::VectorXd noise_x(N);
+  Eigen::VectorXd noise_y(N);
+  std::ofstream file("data.txt");
   for (int i = 0; i < N; i++) {
-    double noise = dist(method);
-    x(i) += noise;
-    y(i) += noise;
+    noise_x(i) = x(i) + dist(method);
+    noise_y(i) = y(i) + dist(method);
+    // x, y, noise_x, noise_yの順でdata.txtに出力する
+    file << i+1 << " " << x(i) << " " << y(i) << " " << noise_x(i) << " " << noise_y(i) << std::endl;
   }
 
+  // ファイルを閉じる
+  file.close();
 
   return 0;
 }
