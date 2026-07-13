cde5e997cfe23d533c863975e622181a514121cb
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-07-13 19:11:40 +0900

    add s_mean and s_var to day4-kadai1

--- day4/kadai1/main.cpp
+++ day4/kadai1/main.cpp
@@ -15,10 +15,28 @@ int main (int argc, char *argv[]) {
   // 正規分布の乱数生成器を初期化
   std::normal_distribution<double> dist(ave, sd);
 
+  // 乱数を格納するベクトルと合計値を格納する変数を定義
+  std::vector<double> data(N);
+  double sum = 0.0;
+
   // N個の乱数を生成して出力
   for (int i = 0; i < N; i++) {
-    std::cout << dist(method) << std::endl;
+    data[i] = dist(method);
+    sum += data[i];
+    std::cout << data[i] << std::endl;
   }
+
+  // 生成した乱数群の標本平均を計算
+  double s_mean = sum / N;
+  std::cout << "標本平均: " << s_mean << std::endl;
+
+  // 生成した乱数群の標本分散を計算
+  double s_var = 0.0;
+  for (int i = 0; i < N; i++) {
+    s_var += (data[i] - s_mean) * (data[i] - s_mean);
+  }
+  s_var /= N;
+  std::cout << "標本分散: " << s_var << std::endl;
 
   return 0;
 }
