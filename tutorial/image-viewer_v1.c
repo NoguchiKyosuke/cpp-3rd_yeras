42a794e0d8243869da07e88fed2c89b349e44ee4
NoguchiKyosuke <k.noguchi2005@gmail.com>
2026-07-27 14:18:46 +0900

    start v2

--- tutorial/image-viewer_v1.c
+++ tutorial/image-viewer_v1.c
@@ -1,20 +1,10 @@
 #include <gtk/gtk.h>
 
 static void
-on_quit (GtkWidget* button, gpointer* user_data) {
-    g_application_quit (G_APPLICATION(user_data));
-}
-
-static void
 on_activate (GApplication* app, gpointer* user_data) {
     GtkWidget* window = gtk_application_window_new (GTK_APPLICATION (app));
     gtk_window_set_title (GTK_WINDOW(window), "Image viewer");
     gtk_window_set_default_size (GTK_WINDOW(window), 400, 300);
-
-    GtkWidget* button = gtk_button_new_with_label ("Quit");
-    g_signal_connect (G_OBJECT(button), "clicked", G_CALLBACK(on_quit), app);
-    gtk_window_set_child (GTK_WINDOW(window), button);
-
     gtk_window_present (GTK_WINDOW(window));
 }
 
